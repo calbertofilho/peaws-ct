ff6ecc9f2ed9c2f7bf4891663b380bb174f4e75f
Carlos Alberto Filho <calbertofilho@gmail.com>
2024-09-20 16:38:02 -0300

    Update 20-09-2024

--- modulo_04-load_balancer/instance/user_dataA.sh
+++ modulo_04-load_balancer/instance/user_dataA.sh
@@ -16,6 +16,9 @@ echo -ne '<!DOCTYPE html>
                 background-color: rgb(40, 42, 54);
                 color: rgb(241, 250, 140);
             }
+            h5 {
+                color: rgb(139, 233, 253)
+            }
         </style>
     </head>
 
@@ -24,6 +27,7 @@ echo -ne '<!DOCTYPE html>
             <h1>W E L C O M E    ! ! !</h1>
             <img src="https://i.imgur.com/U7YYtgL.png" alt="Melted smile faces" width="1000" height="500" />
             <h3>This page was created using Terraform&apos;s user_data script.</h3>
+            <h5>Instance A</h5>
         </center>
     </body>
 </html>' | sudo tee /var/www/html/index.html
--- modulo_04-load_balancer/res_instance-seccurity.tf
+++ modulo_04-load_balancer/res_instance-seccurity.tf
@@ -1,18 +1,3 @@
-resource "aws_instance" "instance_ec2" {
-    ami = data.aws_ami.most_recent_ubuntu_ami.id
-    instance_type = "t2.micro"
-    key_name = "cloudtreinamentos_estudos" # Insira o nome do par de chaves criadas antes
-    subnet_id = aws_subnet.public_subnet[0].id
-    vpc_security_group_ids = [aws_security_group.allow_ssh_http_traffic.id]
-    user_data = "${file("instance/user_data.sh")}"
-    associate_public_ip_address = true
-    tags = merge(local.common_tags, {
-    # tags = {
-        Name = format("%s", local.instance_name)
-    # }
-    })
-}
-
 resource "aws_security_group" "allow_ssh_http_traffic" {
     name = "allow_traffic"
     description = "Allow HTTP/TLS/SSH inbound traffic and all outbound traffic"
