062e30d18e9c7ad0f3da85c6356996cc95f4396c
Carlos Alberto Filho <calbertofilho@gmail.com>
2024-09-20 11:12:55 -0300

    Update 20-09-2024

--- modulo_03-instance/provider.tf
+++ modulo_03-instance/provider.tf
@@ -1,6 +1,5 @@
 # Configure the provider
 provider "aws" {
-    alias = "North Virginia"
     region = var.project_region
     access_key = var.project_accessKey
     secret_key = var.project_secretKey
