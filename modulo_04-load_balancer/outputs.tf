008d20ca18d6c8f9dbab1c85139e3d70689aa4db
Carlos Alberto Filho <calbertofilho@gmail.com>
2024-10-14 17:11:52 -0300

    Update 14-10-2024

--- modulo_04-load_balancer/outputs.tf
+++ modulo_04-load_balancer/outputs.tf
@@ -7,3 +7,8 @@ output "lb_url" {
   description = "URL of load balancer"
   value = "http://${aws_lb.application_elb.dns_name}/"
 }
+
+#output "web_server_count" {
+#    description = "Number of web servers provisioned"
+#    value = length(module.ec2_instances.instance_ids)
+#}
